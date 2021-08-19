92df13232054ae1e5da7547480e7618cea5c7f22
Mathias Rodrigues da Luz <68118121+MathiasRLuz@users.noreply.github.com>
2021-08-19 14:51:38 -0300

    Continuação do jogo

---
+++ PassarosLocos/Assets/Scripts/Monster.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (ShouldDieFromCollision(collision))
+        {
+            Die();
+        }
+    }
+
+    bool ShouldDieFromCollision(Collision2D collision)
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        if (bird != null)
+        {
+            return true;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5) // lê se o primeiro contato foi de algo que veio por cima
+            return true;
+
+        return false;
+    }
+
+    void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
